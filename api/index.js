364311337ba1547120596a0a6af5cae398663541
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-07-18 11:21:44 +0530

    server updated with nodemon dotenv

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
-const express=require('express');
-const port=3000;
+import express from 'express';
+
+const port=process.env.PORT;
 const api=express();
+
+
 api.listen(port,()=>{
     console.log(`listening to port ${port}`);
 });
