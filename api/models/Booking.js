4f0326d9ca18b32c7236948713075de6bcd16f0e
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-07-21 17:43:09 +0530

    Booking schema updated

---
+++ api/models/Booking.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const BookingSchema = new Schema(
+  {
+    ServiceId: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Booking", BookingSchema);
