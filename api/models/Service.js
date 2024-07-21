9d84fb8cacffb335d90be066dade9fa90c8d80a2
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-07-21 17:39:02 +0530

    service schema updated

---
+++ api/models/Service.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const ServiceSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    totalStars: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    deliveryTime: {
+      type: Number,
+      required: true,
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Service", ServiceSchema);
